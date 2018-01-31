d1f5257800f8f80e6cdfa6c4e2704b09bd8a5987
倪俊杰 <1006312908@qq.com>
2018-01-31 23:32:31 +0800

    feat: 加mockjs来做模拟接口数据

--- mock/home/hello.js
+++ mock/home/hello.js
@@ -1,3 +1,15 @@
+// 引入mockjs来模拟数据
+// https://github.com/nuysoft/Mock/wiki/Getting-Started
+const Mock = require('mockjs')
+const data = Mock.mock({
+  'list|1-10': [{
+    'id|+1': 1
+  }]
+})
+const img = Mock.Random.image('200x100')
+
 module.exports = {
-  msg: 'mock hello api works'
+  msg: 'mock hello api works',
+  data: data,
+  imgUrl: img
 }
