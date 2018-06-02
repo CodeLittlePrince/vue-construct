954451ad81cc5706a7db096f8933985d9938142e
倪俊杰 <1006312908@qq.com>
2018-06-03 02:09:54 +0800

    修改e2e配置

--- test/e2e/specs/page.spec.js
+++ test/e2e/specs/page.spec.js
@@ -4,6 +4,9 @@ const expect = chai.expect
 const nightmare = Nightmare({
   show: true 
 })
+// 运行npm run test:e2e http://0.0.0.0:9999
+// 则测试页面就为http://0.0.0.0:9999
+const url = process.argv[3]
 
 describe('pages', () => {
   it('page ', function(done) {
@@ -11,7 +14,7 @@ describe('pages', () => {
     this.timeout('30s')
     nightmare
       .viewport(1200, 600)
-      .goto('http://0.0.0.0:9999')
+      .goto(url || 'http://0.0.0.0:8080')
       .wait('h1')
       .click('a[href="#/pageA"]')
       .wait(() => {
