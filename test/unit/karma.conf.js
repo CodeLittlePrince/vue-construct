4978ef9c383bbe86f062367ec735c32394ad76f6
倪俊杰 <1006312908@qq.com>
2018-10-10 15:35:52 +0800

    单元测试删除端口号

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -46,10 +46,6 @@ module.exports = function(config) {
     },
 
 
-    // web server port
-    port: 9876,
-
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
 
