088d7efdc7afbf98d7a29253cfff12a82ee233cf
倪俊杰 <1006312908@qq.com>
2018-10-24 15:41:34 +0800

    输出优化

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -39,6 +39,8 @@ const config = Object.assign(webpackConfigBase.config, {
     webpackConfigBase.VueLoaderPluginInstance
   ],
   devServer: {
+    clientLogLevel: 'none',
+    quiet: true,
     proxy: {
       // 凡是 `/api` 开头的 http 请求，都会被代理到 target 上，由 koa 提供 mock 数据。
       // koa 代码在 ./mock 目录中，启动命令为 npm run mock。
